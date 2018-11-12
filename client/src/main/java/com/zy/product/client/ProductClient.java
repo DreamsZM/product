928760e7210009a3ee0e9bbfb3e4e63d5917c3e3
zy <zhaomin1423@foxmail.com>
2018-11-12 21:24:40 +0800

    sleuth has some problem

--- client/src/main/java/com/zy/product/client/ProductClient.java
+++ client/src/main/java/com/zy/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.zy.product.client;
 import com.zy.product.common.DecreaseStockInput;
 import com.zy.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,7 +12,7 @@ import java.util.List;
 /**
  * @author Zhao Min
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -19,4 +20,21 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    /**
+     * 服务降级时调用该类的方法
+     */
+    @Component
+    class ProductClientFallback implements ProductClient{
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
